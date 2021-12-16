4f5b341694795b51d29c25473dff96a4b8b0e405
clumsyCoder00 <nuthanael.watkins@gmail.com>
2021-12-16 12:42:42 -0500

    Updates
    - Revised for counterclockwise rotation
    - Added speed control by dial instead of by serial interface
    - Revised second hand motion to smooth instead of increment by each second

--- Harddrive Clock/src/main.cpp
+++ Harddrive Clock/src/main.cpp
@@ -10,16 +10,17 @@ https://techtutorialsx.com/2016/12/11/esp8266-external-interrupts/#comments
 */
 
 // wireless network data
-const char *ssid     = "SSID";
-const char *password = "PASSWORD";
+const char *ssid     = "watkins";
+const char *password = "abc123ab";
 
 const long utcOffsetInSeconds = - 14400; // -4 hrs with daylight harvesting, -18000 without
 
 // pins for hardware interfacing
-int LEDpin = 4;
-const byte interruptPin = 13; //D7
+const byte LEDpin = 12; //D6
+const byte interruptPin = 4; // D2
+const byte servoPin = 2; // D4
 
-void ICACHE_RAM_ATTR handleInterrupt ();
+void IRAM_ATTR detectsRev ();
 void setHand(unsigned long, unsigned long, unsigned long, boolean*, int, boolean, int, boolean, int);
 unsigned long movingAvg(unsigned long*, unsigned long*, uint8_t, uint16_t, unsigned long);
 unsigned long wrap(unsigned long, unsigned long, unsigned long, boolean);
@@ -27,11 +28,12 @@ void speedInput(Servo&);
 
 Servo myMotor;
 String incomingString;
+int lastVal = 0;
 
 volatile byte interruptCounter = 0;
 
-int thisInterrupt = 0;
-int lastInterrupt = 0;
+// int thisInterrupt = 0;
+// int lastInterrupt = 0;
 
 volatile unsigned long clockCount;
 volatile unsigned long lastClockCount;
@@ -46,12 +48,15 @@ unsigned long testEnd;
 
 
 unsigned long revoAvg = 0; // average clock cycles per revolution, rolling average of revoLen values
-const uint8_t revoLen = 6; // number of values used to average clock cycles per revolution
-unsigned long revoArray[revoLen]; // declaring array
-unsigned long revoSum = 0;
-uint8_t revoIndex = 0;
+// const uint8_t revoLen = 6; // number of values used to average clock cycles per revolution
+// unsigned long revoArray[revoLen]; // declaring array
+// unsigned long revoSum = 0;
+// uint8_t revoIndex = 0;
 
-unsigned long printLt = 0;
+// tenths
+unsigned tenths = 0;
+//int secTenths = 0;
+unsigned lastSec = 0;
 
 // seconds hand
 unsigned int sec = 0;       // store time - seconds
@@ -65,7 +70,7 @@ unsigned int mins = 0;
 unsigned long minsStart = 0;
 unsigned long minsStop = 0;
 boolean minsON = false;
-int minsBrt = 256;
+int minsBrt = 105;
 
 // hours hand
 unsigned int hrs = 0;
@@ -85,19 +90,19 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 void setup() {
   Serial.begin(115200);
-  myMotor.attach(0);
+  myMotor.attach(servoPin);
   pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), detectsRev, FALLING);
 	pinMode(LEDpin, OUTPUT);
-  analogWriteFreq(50000); // 1 – 1000Khz
+  analogWriteFreq(25000); // 1 – 1000Khz
 
   // get the motor started, ESC needs full throttle, then zero throttle signal to arm
-  delay(3000);
+  delay(1500);
   myMotor.write(180);
-  delay(3000);
+  delay(750);
   myMotor.write(0);
-  delay(3000);
-  myMotor.write(73);
+  delay(750);
+  myMotor.write(55);
 
 // NTP
   WiFi.begin(ssid, password);
@@ -112,19 +117,6 @@ void setup() {
 
 void loop() {
   thisClockCount = ESP.getCycleCount();
-
-  // thisInterrupt = digitalRead(interruptPin);
-  // if (thisInterrupt != lastInterrupt)
-  // {
-  //   if (thisInterrupt == 0)
-  //   {
-  //     interruptCounter++;
-  //     lastInterrupt = 0;
-  //     clockCount = thisClockCount;
-  //   }
-  //   else
-  //     lastInterrupt = 1;
-  // }
   
   if(interruptCounter>0){
     interruptCounter--;
@@ -132,44 +124,56 @@ void loop() {
     lastClockCount = clockCount;
     // testStart = ESP.getCycleCount();
     // revoAvg = movingAvg(revoArray, &revoSum, revoIndex, revoLen, revoTime);
+    // revoIndex = (revoIndex + 1) % revoLen;
     revoAvg = revoTime;
-    revoIndex = (revoIndex + 1) % revoLen;
-    offset = revoAvg/20;
+    // revoAvg = 4 000 000 when spinning as slow as possible
+    offset = revoAvg * 0.28; // higher, farther counter-clockwise .22 ahead by 3 sec .25 ahead by 1 sec
     // testEnd = ESP.getCycleCount();
   }
-  
-  // if((thisClockCount - printLt) >= 26){
-    // printLt = thisClockCount;
-    setHand(thisClockCount-clockCount, secStart, secStop, &secON, secBrt, minsON, minsBrt, hrsON, hrsBrt);
-    setHand(thisClockCount-clockCount, minsStart, minsStop, &minsON, minsBrt, secON, secBrt, hrsON, hrsBrt);
-    setHand(thisClockCount-clockCount, hrsStart, hrsStop, &hrsON, hrsBrt, secON, secBrt, minsON, minsBrt);
-  // }
 
-  if((thisClockCount - i) >= 80000000) {
+  setHand(thisClockCount-clockCount, secStart, secStop, &secON, secBrt, minsON, minsBrt, hrsON, hrsBrt);
+  setHand(thisClockCount-clockCount, minsStart, minsStop, &minsON, minsBrt, secON, secBrt, hrsON, hrsBrt);
+  setHand(thisClockCount-clockCount, hrsStart, hrsStop, &hrsON, hrsBrt, secON, secBrt, minsON, minsBrt);
+
+  // 80 000 000 = once per second
+  // 8 000 000 = ten times per second
+  // 4 000 000 = twenty times per second
+  if((thisClockCount - i) >= 4000000) {
     if(timeClient.update()){
       i = thisClockCount;
 
-      sec = timeClient.getSeconds();
-      // sec = 45;
-      secStart = wrap((revoAvg * sec)/60 + offset, revoAvg / 480, revoAvg, false);
-      secStop  = wrap((revoAvg * sec)/60 + offset, revoAvg / 480, revoAvg,  true);
+      sec = 59 - timeClient.getSeconds();
 
-      mins = timeClient.getMinutes();
-      // mins = 15;
+      if(sec != lastSec){
+        lastSec=sec;
+        tenths=0;
+      }
+      else
+        tenths = (tenths + 1) % 20;
+      
+      // sec = 15;
+      //secTenths = sec + tenths; // increment in range from 0 - 9
+      // Serial.println((revoAvg/600) * tenths);
+      secStart = wrap((revoAvg * sec)/60 - ((revoAvg/1200) * tenths) + offset, revoAvg / 480, revoAvg, false);
+      secStop  = wrap((revoAvg * sec)/60 - ((revoAvg/1200) * tenths) + offset, revoAvg / 480, revoAvg,  true);
+
+      mins = 59 - timeClient.getMinutes();
+      // mins = 0;
       minsStart = wrap(((revoAvg * mins) / 60) + ((revoAvg * sec) / 3600) + offset, revoAvg / 120, revoAvg, false);
       minsStop  = wrap(((revoAvg * mins) / 60) + ((revoAvg * sec) / 3600) + offset, revoAvg / 120, revoAvg, true);
 
-      hrs = (timeClient.getHours()%12);
+      hrs = 12 - (timeClient.getHours()%12);
       // hrs = 6;
       hrsStart = wrap(((revoAvg * hrs) / 12) + ((revoAvg * mins) / 720) + ((revoAvg * sec) / 43200) + offset, revoAvg / 24, revoAvg, false);
       hrsStop  = wrap(((revoAvg * hrs) / 12) + ((revoAvg * mins) / 720) + ((revoAvg * sec) / 43200) + offset, revoAvg / 24, revoAvg, true);
     }
+
+    // range: 62-145
+    // 700 - 8900 rpm
+    speedInput(myMotor);
+    // Serial.print("test: ");
     // Serial.println(testEnd - testStart);
   }
-
-  // range: 62-145
-  // 700 - 8900 rpm
-  speedInput(myMotor);
 }
 
 unsigned long wrap(unsigned long center, unsigned long wing, unsigned long ceiling, boolean additive){
@@ -236,14 +240,12 @@ void setHand(unsigned long timeAtCall, unsigned long startTime, unsigned long st
             analogWrite(LEDpin, other1Brt);
           else
             analogWrite(LEDpin, other2Brt);
-      
       }
     }
   }
-  // interrupts();
 }
 
-void handleInterrupt() {
+IRAM_ATTR void detectsRev() {
   interruptCounter++;
   clockCount=ESP.getCycleCount();
 }
@@ -259,26 +261,12 @@ unsigned long movingAvg(unsigned long *ptrArrNumbers, unsigned long *ptrSum, uin
 }
 
 void speedInput(Servo& myMotor) {
-  if(Serial.available() > 0)
-  {
-    char ch = Serial.read();
-    if (ch != 10){
-      incomingString += ch;
-    }
-    else
-    {
-      Serial.println(incomingString);
-      int val = incomingString.toInt();
-      if (val > -1 && val < 181)
-     {
-       myMotor.write(val);
-     }
-     else
-     {
-       Serial.println("Value is NOT between 0 and 180");
-       Serial.println("Error with the input");
-     }
-      incomingString = "";
-    }
+  // 7 - 1024
+  // 60*1/(4600000/80000000) = RPM
+  int val = map(analogRead(A0),1024,7,54,180);
+  if (val != lastVal){
+    lastVal = val;
+    myMotor.write(val);
+    // Serial.println(analogRead(A0));
   }
 }
